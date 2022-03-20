dc71e80edb7eb93c686be0deea98f082b67f6ecc
aliterliksiz <aliterliksiz46@gmail.com>
2022-03-21 00:28:53 +0300

    StaticWebSiteMVC Project Added

---
+++ StaticWebSiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebSiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("hakkimizda")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("blog")]
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        [Route("iletisim")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
